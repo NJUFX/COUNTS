43b4f73256feb47a167837072332eb288f7b60d0
161250025 <161250025@smail.nju.edu.cn>
2018-06-06 16:08:42 +0800

    update

--- Browser/src/components/Message.vue
+++ Browser/src/components/Message.vue
@@ -4,8 +4,8 @@
         <happy-scroll class="happy_scroll" color="rgba(51,51,51,0.2)" hide-vertical="false">
           <div v-for="item in messageList" :key="item">
             <div class="info" @click="showInfo(item.index)">
-              <span style="position:absolute; font-size: 16px; top: 5px; font-weight: bold;left: 5px; text-align: left;width: 200px; overflow: hidden; text-overflow:ellipsis;white-space: nowrap;">{{item.topic}}</span>
-              <p style="position: absolute; font-size: 14px; top: 10px;overflow: hidden; text-overflow:ellipsis;white-space: nowrap; width: 275px; left: 5px">{{item.content}}</p>
+              <span style="position:absolute; font-size: 15px; top: 3px; font-weight: bold;left: 5px; text-align: left;width: 200px; overflow: hidden; text-overflow:ellipsis;white-space: nowrap;">{{item.topic}}</span>
+              <p style="position: absolute; font-size: 12px; top: 12px;overflow: hidden; text-overflow:ellipsis;white-space: nowrap; width: 275px; left: 5px">{{item.content}}</p>
               <div v-show="item.unread" style="background-color: #ff732f; position: absolute;right: 6px; top: 10px; width: 6px;height: 6px;border-radius: 50%"></div>
             </div>
           </div>
@@ -15,6 +15,7 @@
       <div style="position: absolute; width: 800px;top: 130px; left: 330px; height: 350px;">
         <span style="font-size: 24px">{{topic}}</span>
         <p style="top: 150px; text-align: justify;font-size: 16px;">{{mainInfo}}</p>
+        <el-button v-show="goProject" type="text" style="font-size: 16px">点此进入项目</el-button>
       </div>
     </div>
 </template>
@@ -24,6 +25,8 @@
     name: "message",
     data(){
       return{
+        topic: '',
+        goProject:false,
         mainInfo: '',
         messageList:[]
       }
@@ -32,30 +35,10 @@
       for(var i=0;i<15;i++){
         this.messageList.push({
           index: i,
-          topic: 'topic '+i,
-          content: '<el-badge :value="12" class="item">\n' +
-          '  <el-button size="small">评论</el-button>\n' +
-          '</el-badge>\n' +
-          '<el-badge :value="3" class="item">\n' +
-          '  <el-button size="small">回复</el-button>\n' +
-          '</el-badge>\n' +
-          '\n' +
-          '<el-dropdown trigger="click">\n' +
-          '  <span class="el-dropdown-link">\n' +
-          '    点我查看<i class="el-icon-caret-bottom el-icon--right"></i>\n' +
-          '  </span>\n' +
-          '  <el-dropdown-menu slot="dropdown">\n' +
-          '    <el-dropdown-item class="clearfix">\n' +
-          '      评论\n' +
-          '      <el-badge class="mark" :value="12" />\n' +
-          '    </el-dropdown-item>\n' +
-          '    <el-dropdown-item class="clearfix">\n' +
-          '      回复\n' +
-          '      <el-badge class="mark" :value="3" />\n' +
-          '    </el-dropdown-item>\n' +
-          '  </el-dropdown-menu>\n' +
-          '</el-dropdown>' ,
+          topic: '【自动化标注 审查】',
+          content: "自动化项目已添加到您的项目列表中，您需要对自动化标注的结果进行审查。\n审查过程：对每张图片的结果正确与否选择“是”或“否”。",
           unread:true,
+
         })
       }
     },
@@ -64,6 +47,7 @@
         this.topic = this.messageList[i].topic;
         this.mainInfo = this.messageList[i].content;
         this.messageList[i].unread = false;
+        this.goProject = true;
       }
     }
   }
