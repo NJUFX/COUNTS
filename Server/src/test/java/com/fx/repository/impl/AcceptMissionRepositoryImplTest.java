e809658a1e7ec08de8c8ecf57ac614fc6bead5b3
161250036 <161250036@smail.nju.edu.cn>
2018-06-15 18:51:10 +0800

    test

---
+++ Server/src/test/java/com/fx/repository/impl/AcceptMissionRepositoryImplTest.java
@@ -0,0 +1,29 @@
+package com.fx.repository.impl;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Description:
+ * Created by Hanxinhu at 18:43 2018/6/15/015
+ */
+public class AcceptMissionRepositoryImplTest {
+
+    @Test
+    public void addAcceptMission() {
+
+    }
+
+    @Test
+    public void updateAcceptMission() {
+    }
+
+    @Test
+    public void findAcceptMissionByUsername() {
+    }
+
+    @Test
+    public void getAllAcceptMission() {
+    }
+}
