03add9f0b936b7815482b8d8391fe761e8257b04
161250080 <161250080@smail.nju.edu.cn>
2018-06-21 20:40:45 +0800

    no message

--- Browser/src/components/Admin.vue
+++ Browser/src/components/Admin.vue
@@ -71,7 +71,7 @@
       </el-card>
 
       <el-card class="card">
-        <div id="LineChart" style="width:100%; height:410px;"></div>
+        <div id="totalUserChange" style="width:100%; height:410px;"></div>
       </el-card>
 
     </div>
@@ -1220,8 +1220,66 @@ export default {
       //option.series[0].data = myData
       myChart.setOption(option)
     },
-    drawLineChart(){
+    drawTotalUserChange(){
+      var myChart = echarts.init(document.getElementById('totalUserChange'))
+      var dataList = []
+      var valueList = []
+      var xmlhttp = new XMLHttpRequest()
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
 
+        }
+      }
+      xmlhttp.open('GET', 'http://localhost:8080/counts/analysis/predictchart', false)
+      xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+      xmlhttp.send(null)
+    var  data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
+
+      var dateList = data.map(function (item) {
+        return item[0];
+      });
+      var valueList = data.map(function (item) {
+        return item[1];
+      });
+
+      var option = {
+
+        // Make gradient line here
+        visualMap: [{
+          show: false,
+          type: 'continuous',
+          seriesIndex: 0,
+          min: 0,
+          max: 400
+        }, ],
+
+
+        title: [{
+          top: '5%',
+          left: 'center',
+          text: '总用户数变化图'
+        },],
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: [{
+          data: dateList
+        }],
+        yAxis: [{
+          splitLine: {show: false}
+        }, ],
+        grid: [{
+          bottom: '30%'
+        }, {
+          top: '60%'
+        }],
+        series: [{
+          type: 'line',
+          showSymbol: false,
+          data: valueList
+        }, ]
+      };
+      myChart.setOption(option)
     },
 
   },
@@ -1233,7 +1291,7 @@ export default {
     this.drawUserDistribution()
     this.drawMissionNum()
     this.drawBoxChart()
-    this.drawLineChart()
+    this.drawTotalUserChange()
   }
 }
 </script>
