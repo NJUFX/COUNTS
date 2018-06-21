1f8e759257bbe4bf8f18182a23d8a277e951e5a8
161250080 <161250080@smail.nju.edu.cn>
2018-06-21 21:05:30 +0800

    no message

--- Browser/src/components/Admin.vue
+++ Browser/src/components/Admin.vue
@@ -37,6 +37,9 @@
             <el-menu-item-group>
               <el-menu-item index="3-1">用户流失阈值率</el-menu-item>
               <el-menu-item index="3-2">用户分流标注数量统计</el-menu-item>
+              <el-menu-item index="3-3">总用户数变化图</el-menu-item>
+              <el-menu-item index="3-4">活跃用户数变化图</el-menu-item>
+              <el-menu-item index="3-5">新增用户数变化图</el-menu-item>
             </el-menu-item-group>
           </el-submenu>
 
@@ -69,9 +72,14 @@
       <el-card class="card">
         <div id="boxChart" style="width:100%; height:410px;"></div>
       </el-card>
-
       <el-card class="card">
         <div id="totalUserChange" style="width:100%; height:410px;"></div>
+      </el-card>
+      <el-card class="card">
+        <div id="activeUserChange" style="width:100%; height:410px;"></div>
+      </el-card>
+      <el-card class="card">
+        <div id="newUserChange" style="width:100%; height:410px;"></div>
       </el-card>
 
     </div>
@@ -120,6 +128,18 @@ export default {
         }
         case '3-2': {
           window.scrollTo(0, 3060)
+          break
+        }
+        case '3-3': {
+          window.scrollTo(0, 3560)
+          break
+        }
+        case '3-4': {
+          window.scrollTo(0, 4060)
+          break
+        }
+        case '3-5': {
+          window.scrollTo(0, 4560)
           break
         }
       }
@@ -1222,25 +1242,19 @@ export default {
     },
     drawTotalUserChange(){
       var myChart = echarts.init(document.getElementById('totalUserChange'))
-      var dataList = []
+      var dateList = []
       var valueList = []
       var xmlhttp = new XMLHttpRequest()
       xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-
+          console.log(JSON.parse(xmlhttp.responseText))
+          dateList  = JSON.parse(xmlhttp.responseText).x
+          valueList = JSON.parse(xmlhttp.responseText).y
         }
       }
-      xmlhttp.open('GET', 'http://localhost:8080/counts/analysis/predictchart', false)
+      xmlhttp.open('GET', 'http://localhost:8080/counts/analysis//MemberLine', false)
       xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
       xmlhttp.send(null)
-    var  data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
-
-      var dateList = data.map(function (item) {
-        return item[0];
-      });
-      var valueList = data.map(function (item) {
-        return item[1];
-      });
 
       var option = {
 
@@ -1281,6 +1295,116 @@ export default {
       };
       myChart.setOption(option)
     },
+    drawActiveUserChange(){
+      var myChart = echarts.init(document.getElementById('activeUserChange'))
+      var dateList = []
+      var valueList = []
+      var xmlhttp = new XMLHttpRequest()
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          console.log(JSON.parse(xmlhttp.responseText))
+          dateList  = JSON.parse(xmlhttp.responseText).x
+          valueList = JSON.parse(xmlhttp.responseText).y
+        }
+      }
+      xmlhttp.open('GET', 'http://localhost:8080/counts/analysis//activeMemberLine', false)
+      xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+      xmlhttp.send(null)
+
+      var option = {
+
+        // Make gradient line here
+        visualMap: [{
+          show: false,
+          type: 'continuous',
+          seriesIndex: 0,
+          min: 0,
+          max: 400
+        }, ],
+
+
+        title: [{
+          top: '5%',
+          left: 'center',
+          text: '活跃用户数变化图'
+        },],
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: [{
+          data: dateList
+        }],
+        yAxis: [{
+          splitLine: {show: false}
+        }, ],
+        grid: [{
+          bottom: '30%'
+        }, {
+          top: '60%'
+        }],
+        series: [{
+          type: 'line',
+          showSymbol: false,
+          data: valueList
+        }, ]
+      };
+      myChart.setOption(option)
+    },
+    drawNewUserChange(){
+      var myChart = echarts.init(document.getElementById('newUserChange'))
+      var dateList = []
+      var valueList = []
+      var xmlhttp = new XMLHttpRequest()
+      xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+          console.log(JSON.parse(xmlhttp.responseText))
+          dateList  = JSON.parse(xmlhttp.responseText).x
+          valueList = JSON.parse(xmlhttp.responseText).y
+        }
+      }
+      xmlhttp.open('GET', 'http://localhost:8080/counts/analysis//newMemberLine', false)
+      xmlhttp.setRequestHeader('Content-type', 'application/json; charset=utf-8')
+      xmlhttp.send(null)
+
+      var option = {
+
+        // Make gradient line here
+        visualMap: [{
+          show: false,
+          type: 'continuous',
+          seriesIndex: 0,
+          min: 0,
+          max: 400
+        }, ],
+
+
+        title: [{
+          top: '5%',
+          left: 'center',
+          text: '新增用户数变化图'
+        },],
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: [{
+          data: dateList
+        }],
+        yAxis: [{
+          splitLine: {show: false}
+        }, ],
+        grid: [{
+          bottom: '30%'
+        }, {
+          top: '60%'
+        }],
+        series: [{
+          type: 'line',
+          showSymbol: false,
+          data: valueList
+        }, ]
+      };
+      myChart.setOption(option)
+    },
 
   },
   mounted: function () {
@@ -1292,6 +1416,8 @@ export default {
     this.drawMissionNum()
     this.drawBoxChart()
     this.drawTotalUserChange()
+    this.drawActiveUserChange()
+    this.drawNewUserChange()
   }
 }
 </script>
